270573574752a54323994cb24408ceaa3a4511bd
Matthew Caruana Galizia <mattcg@gmail.com>
2013-11-05 20:40:08 -0600

    Silly. Caching logic was in the wrong place.

--- controllers/shared/cedulas.js
+++ controllers/shared/cedulas.js
@@ -21,32 +21,15 @@ exports.scrape = function(type, cedula, scrape, res) {
 		logger.info('[' + type + '] Cache miss for ' + cedula + '.');
 
 		scrape(app, cedula, function(err, results) {
+			var cacheTtl;
+
 			if (err) {
 				logger.error('[' + type + '] Error while scraping ' + cedula + ': ' + err);
 
 				res.json(502, err);
-			} else if (!empty(results)) {
-				res.json(results);
-			} else {
-				res.send(404);
+
+				return;
 			}
-		});
-	};
-
-	if (!cache) {
-		logger.warn('[' + type + '] Warning: not using cache.');
-		return cacheMiss();
-	}
-
-	cache.get(key(cedula, type), function(err, results) {
-		var cacheTtl;
-
-		if (err) {
-			logger.error('[' + type + '] Error while getting value from cache for ' + cedula + ': ' + err);
-
-			res.json(502, err);
-		} else if (results) {
-			results = JSON.parse(results);
 
 			if (cache) {
 				cacheTtl = app.get('cache ttl');
@@ -61,11 +44,34 @@ exports.scrape = function(type, cedula, scrape, res) {
 			}
 
 			if (!empty(results)) {
-				logger.info('[' + type + '] Got results from cache for ' + cedula + '.');
-
 				res.json(results);
 			} else {
 				res.send(404);
+			}
+		});
+	};
+
+	if (!cache) {
+		logger.warn('[' + type + '] Warning: not using cache.');
+		return cacheMiss();
+	}
+
+	cache.get(key(cedula, type), function(err, results) {
+		if (err) {
+			logger.error('[' + type + '] Error while getting value from cache for ' + cedula + ': ' + err);
+
+			res.json(502, err);
+		} else if (results) {
+			results = JSON.parse(results);
+
+			if (empty(results)) {
+				logger.info('[' + type + '] Got (empty) results from cache for ' + cedula + '.');
+
+				res.send(404);
+			} else {
+				logger.info('[' + type + '] Got results from cache for ' + cedula + '.');
+
+				res.json(results);
 			}
 		} else {
 			cacheMiss();
